f96dd066f22e22bd2fdc60b39c1761afccf5981d
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-17 19:24:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def introduction(name)
-  puts "Hi, my name is #{name} ."
+  puts "Hi, my name is #{name}."
 end
 
-introduction( "Josh" )
+introduction("Josh")
 binding.pry
 
 def introduction_with_language(name, language)
